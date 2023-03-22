ac0f41cf91ee280149d46d0723dd5ffc5dc19805
jschj <joe.sch@protonmail.com>
2023-03-22 09:24:35 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "firrtlpp.hpp"
+
+
+int main(int argc, const char **argv)
+{
+  std::unique_ptr<mlir::MLIRContext> context = std::make_unique<mlir::MLIRContext>();
+
+  assert(context->getOrLoadDialect<circt::hw::HWDialect>());
+  assert(context->getOrLoadDialect<circt::seq::SeqDialect>());
+  assert(context->getOrLoadDialect<circt::firrtl::FIRRTLDialect>());
+
+  using namespace ::firrtlpp;
+  using namespace ::circt::firrtl;
+  using namespace ::mlir;
+
+  Value clk;
+  initPrimitiveBuilder(context.get(), clk);
+
+  /*
+  auto valid = UInt(1, 1);
+  auto ready = UInt(1, 1);
+  auto count = UInt(123, 16);
+
+  Value canEnqueue = (valid & ready & (count < lift(constant(8, 16))))->build();
+
+  auto lower = bits(count, 3, 0);
+  auto someField = field(lower, "someField");
+
+  auto reg = Reg(UInt(32));
+  reg << count + count;
+  reg << count + count;
+   */
+
+  //auto testModule = TestModule();
+  auto queue = TestModuleA(); // Queue(UInt(32));
+
+  assert(succeeded(getPrimitiveBuilder()->root.verify()));
+  getPrimitiveBuilder()->dump();
+
+  return 0;
+}
